ef55255176b676dd24e2866b704214f2c3b7980f
Anqur <anqurvanillapy@gmail.com>
2024-01-19 15:20:47 +0800

    Unification
    Signed-off-by: Anqur <anqurvanillapy@gmail.com>

--- lyzh/abstract/unify.py
+++ lyzh/abstract/unify.py
@@ -1,6 +1,30 @@
 import dataclasses
 
+import lyzh.abstract.data as ast
+import lyzh.core as core
+import lyzh.abstract.normalize as normalize
+
 
 @dataclasses.dataclass
 class Unifier:
-    pass
+    ids: core.IDs
+    globals: ast.Globals
+
+    def unify(self, lhs: ast.Term, rhs: ast.Term) -> bool:
+        match lhs, rhs:
+            case ast.Ref(x), ast.Ref(y):
+                return x.text == y.text and x.id == y.id
+            case ast.App(f, x), ast.App(g, y):
+                return self.unify(f, g) and self.unify(x, y)
+            case ast.Fn(p, b), ast.Fn(q, c):
+                return self.unify(b, self.nf().term_with((q.name, ast.Ref(p.name)), c))
+            case ast.FnType(p, b), ast.FnType(q, c):
+                if not self.unify(p.type, q.type):
+                    return False
+                return self.unify(b, self.nf().term_with((q.name, ast.Ref(p.name)), c))
+            case ast.Univ(), ast.Univ():
+                return True
+        return False
+
+    def nf(self) -> normalize.Normalizer:
+        return normalize.Normalizer(self.ids, self.globals)
